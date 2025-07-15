eb72c522eddb6379426b2b1aa8257bcb2e603479
MaverickDesigns <164984240+MaverickDesigns@users.noreply.github.com>
2025-07-15 10:14:29 +0530

    Update maintenance.jsx

--- src/pages/maintenance.jsx
+++ src/pages/maintenance.jsx
@@ -1,16 +1,1 @@
-import React from 'react';
-
-const Maintenance = () => {
-  return (
-    <div className="maintenance-container" style={{ width: '100vw' }}>
-      <div className="maintenance-content">
-        <h1>Coming Soon</h1>
-        <p>We're working hard to launch our new portfolio website.</p>
-        <p>Stay tuned!</p>
-      </div>
-    </div>
-  );
-};
-
-export default Maintenance;
 
